cae40c721529c2d9979649f847c5617928aa6471
Clitan <yan.clitan.com>
2019-08-27 12:57:17 +0530

    Admin module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminDashBoardComponent } from './admin/admin-dash-board/admin-dash-board.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'admin/usr', component: AdminDashBoardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
