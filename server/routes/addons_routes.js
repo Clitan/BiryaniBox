bc738e53b58f26136b483477f3bc0837839e4939
SubrayaNayak <subrayanayak22@gmail.com>
2019-09-05 14:19:32 +0530

    added delivery-boy

--- server/routes/addons_routes.js
+++ server/routes/addons_routes.js
@@ -1,48 +1,48 @@
 require('babel-polyfill');
 import {
-    Addon,
-    validate
+  Addon,
+  validate
 } from '../model/addons.model';
 import express from 'express';
 const router = express.Router();
 import BaseController from '../controllers/BaseController';
 import {
-    resError
+  resError
 } from '../helper/http_handler.helper';
 const AddonControllerClass = new BaseController(Addon);
 
 router.post('/', async (req, res) => {
-    const {
-        error
-    } = validate(req.body);
-    if (error) return resError(res, error.details[0].message);
-    await AddonControllerClass.create(req, res);
+  const {
+    error
+  } = validate(req.body);
+  if (error) return resError(res, error.details[0].message);
+  await AddonControllerClass.create(req, res);
 });
 
 router.get('/', async (req, res) => {
-    await AddonControllerClass.getAll(req, res);
+  await AddonControllerClass.getAll(req, res);
 });
 
 router.get('/:id', async (req, res) => {
-    const addons = await Addon.findById(req.params.id);
-    if (!addons) return resError(res, 'The Addon with the given ID was not found.');
-    await AddonControllerClass.getOne(req, res);
+  const addons = await Addon.findById(req.params.id);
+  if (!addons) return resError(res, 'The Addon with the given ID was not found.');
+  await AddonControllerClass.getOne(req, res);
 });
 
 router.put('/:id', async (req, res) => {
-    const {
-        error
-    } = validate(req.body);
-    if (error) return resError(res, error.details[0].message);
-    const addons = await Addon.findById(req.params.id);
-    if (!addons) return resError(res, 'The Addon with the given ID was not found.');
-    await AddonControllerClass.update(req, res);
+  const {
+    error
+  } = validate(req.body);
+  if (error) return resError(res, error.details[0].message);
+  const addons = await Addon.findById(req.params.id);
+  if (!addons) return resError(res, 'The Addon with the given ID was not found.');
+  await AddonControllerClass.update(req, res);
 });
 
 router.delete('/:id', async (req, res) => {
-    const addons = await Addon.findById(req.params.id);
-    if (!addons) return resError(res, 'The Addon with the given ID was not found.');
-    await AddonControllerClass.remove(req, res);
+  const addons = await Addon.findById(req.params.id);
+  if (!addons) return resError(res, 'The Addon with the given ID was not found.');
+  await AddonControllerClass.remove(req, res);
 });
 
 module.exports = router;
